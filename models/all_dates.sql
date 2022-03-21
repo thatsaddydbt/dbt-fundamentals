a6e6a9390deb4d58b09f79b3d66e62b3979f3bf9
thatsaddydbt@gmail.com <thatsaddydbt@gmail.com>
2022-03-21 08:45:08 +0000

    comma

--- models/all_dates.sql
+++ models/all_dates.sql
@@ -4,7 +4,7 @@
 
 
 {{ dbt_utils.date_spine(
-    datepart="day"
-    start_date="cast('2020-01-01' as date)"
+    datepart="day",
+    start_date="cast('2020-01-01' as date)",
     end_date="cast('2021-01-01' as date)"
 )}}
